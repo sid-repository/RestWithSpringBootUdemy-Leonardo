d141cc837ad515c3a76693758b5ff2cb41579999
sid-repository <siddhartha.agrawal.eee15@itbhu.ac.in>
2021-08-23 21:00:18 +0530

    Hello SpringBoot and REST Udemy - Leonardo

---
+++ RestWithSpringBootUdemy-Leonardo Workspace/rest-with-springboot-udemy/src/main/java/springboot/leonardo/Greeting.java
@@ -0,0 +1,21 @@
+package springboot.leonardo;
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+}
